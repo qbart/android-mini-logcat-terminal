3a71f45c5fc484273ce3f893d143183fd1bf9a25
bart <wojtowicz.bartlomiej@gmail.com>
2013-04-19 13:53:08 +0200

    support for multiple devices

--- src/main.rb
+++ src/main.rb
@@ -4,6 +4,48 @@ require './logcat'
 class LogReader
   
   def initialize
+
+  end
+
+  def pick_device
+    @device = nil
+    result = `adb devices`
+
+    if result
+      devices = []
+      devices_result = result.strip.split("\n")
+      devices_result.each do |dr|
+        device = dr.split("\t")
+        if device.size == 2 && device[1] == "device"
+          devices << device.first
+        end
+      end
+
+      if devices.size > 0
+        if devices.size == 1
+          @device = devices.first
+        else
+          puts "Choose device:"
+          devices.each_with_index do |dev, index|
+            puts "#{index+1}. #{dev}"
+          end
+
+          choice = gets.to_i
+
+          if choice >= 1 && choice <= devices.size
+            @device = devices[choice - 1]
+            puts "\nYour choice: #{@device}"
+          else
+            puts "Wrong choice!!!"
+          end
+        end
+      end
+    end
+
+    @device
+  end
+
+  def init
     @break = false
     @mutex = Mutex.new
     @mutex.lock
@@ -16,17 +58,18 @@ class LogReader
   end
 
   def start
+    init
     @mutex.unlock
   end
 
   # read from adb
   def run
     log = Logcat.new
-    log.reset "> Starting logcat.."
+    log.reset "> Starting logcat for device: #{@device}.."
     while !@break do
       # TODO -b events
 
-      @io = IO.popen "adb logcat", "r" do |io|
+      @io = IO.popen "adb -s #{@device} logcat", "r" do |io|
         io.each do |line|
           unless @pause
             log.process_line(line, @filter) unless @break
@@ -92,7 +135,9 @@ for i in 1 .. 15  # SIGHUP .. SIGTERM
   end
 end
 
-reader.start
-reader.listen_user_input
+if reader.pick_device
+  reader.start
+  reader.listen_user_input
+end
 
 
